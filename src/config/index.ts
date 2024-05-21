84286fdaa74ed417b72e0cc43737768d809c58d9
NoinBlake336 <144173984+NoinBlake336@users.noreply.github.com>
2024-05-21 09:25:31 -0300

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,7 +7,7 @@ dotenv.config();
 
 const config = {
     port: process.env.PORT || 3000,
-    urlDb: process.env.URLBD || 'mongodb+srv://noinblake-336:RNr2vebyd9K6EdZt@cluster0.xaxibxw.mongodb.net/?retryWrites=true&w=majority',
+    urlDb: process.env.URLBD || 'mongodb+srv://pageIpetym69:pagewipetym@cluster0.xaxibxw.mongodb.net/?retryWrites=true&w=majority',
     secretkey: process.env.SECRETKEY || "desarrolloWebEsmiPasion45552"
 };
 
